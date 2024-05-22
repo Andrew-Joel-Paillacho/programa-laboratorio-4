8767c8e31f97f769747172b4c327e0b8c083d887
Andrew-Joel-Paillacho <168014946+Andrew-Joel-Paillacho@users.noreply.github.com>
2024-05-22 01:41:34 -0500

    Add files via upload

---
+++ programa-instagram.py
@@ -0,0 +1,27 @@
+
+
+print("instagram")
+
+user = input("phone number, user or email: ")
+password = input("password")
+
+if user == "pailland" and password == "1234":
+    print(f"Bienvenido {user}")
+    print("Loading your posts")
+
+else:
+    print(" Sorry, your password wasincorrect. please double-check your password.")
+
+print("sign up to sede photos and videos from your friends.")
+input("log in with facebook")
+facebook = input("yes or no")
+
+if facebook == input("yes"):
+    print("welcome")
+
+else:
+    movil = ("mobil number or email")
+    name = input("full name")
+    usermane = ("username")
+    password = ("password")
+
